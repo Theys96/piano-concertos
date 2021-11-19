7db9ad4a6fcc5c64a5b8f79a699c80ac9a387f55
Thijs Havinga <accounts@thijshavinga.nl>
2021-11-19 21:00:02 +0100

    Notebook

---
+++ creds.py
@@ -0,0 +1,5 @@
+# Spotify API credentials, both of these are 32-character hashes.
+# Generate them at https://developer.spotify.com/dashboard/applications
+spotify_username = ""
+spotify_password = ""
+
